68da7db8b4e89e0decafc0618099ef699c6fef16
JoergZ2 <joerg.zywek@gmx.de>
2025-02-21 17:03:02 +0100

    Update 20250221

--- __init__.py
+++ __init__.py
@@ -212,9 +212,14 @@ class SamsungTVCtl(OVOSSkill):
     @intent_handler('channel.by.number.intent')
     def handle_channel_by_name(self, message):
         utt = message.data.get('utterance')
-        LOG.info("Inhalt von message: " + str(utt))
-        channel = message.data.get('channel')
-        self.switch_by_channel_number(channel)
+        if "sender" in utt or "kanal" in utt:
+            LOG.info("Inhalt von message: " + str(utt))
+            channel = message.data.get('channel')
+            self.switch_by_channel_number(channel)
+        else:
+            channel = message.data.get('channel')
+            self.switch_by_channel_name(channel)
+
 
     @intent_handler('next_channel.intent')
     def handle_next_channel(self):
